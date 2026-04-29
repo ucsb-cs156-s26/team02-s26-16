c32857934580ab4c4f97efdee4c6baa8fc9809e0
Nikita Huynh <nikitahuynh@ucsb.edu>
2026-04-29 01:45:26 -0700

    nh - create fixtures for ucsbOrganizations table

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizationFixtures = {
+  pfc: {
+
+    orgCode: "tt",
+    orgTranslationShort: "TT",
+    orgTranslation: "Theta Tau",
+    inactive: true
+  },
+  asianOrgs: [
+    {
+      orgCode: "vsa",
+      orgTranslationShort: "VSA",
+      orgTranslation: "Vietnamese Student Association",
+      inactive: true,
+    },
+    {
+      orgCode: "csu",
+      orgTranslationShort: "CSU",
+      orgTranslation: "Chinese Student Union",
+      inactive: true,
+    },
+    {
+      orgCode: "nsu",
+      orgTranslationShort: "NSU",
+      orgTranslation: "Japanese Student Association",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
