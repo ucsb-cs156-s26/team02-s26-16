2b9de7c1b9a793722532afdec0e87428deb93c60
Sharanya Gehlot <sgehlot@ucsb.edu>
2026-04-29 21:50:33 -0700

    Fix Prettier formatting in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -93,24 +93,24 @@ function App() {
       )}
 
       {hasRole(currentUser, "ROLE_USER") && (
-  <>
-    <Route exact path="/articles" element={<ArticlesIndexPage />} />
-  </>
-)}
-{hasRole(currentUser, "ROLE_ADMIN") && (
-  <>
-    <Route
-      exact
-      path="/articles/edit/:id"
-      element={<ArticlesEditPage />}
-    />
-    <Route
-      exact
-      path="/articles/create"
-      element={<ArticlesCreatePage />}
-    />
-  </>
-)}
+        <>
+          <Route exact path="/articles" element={<ArticlesIndexPage />} />
+        </>
+      )}
+      {hasRole(currentUser, "ROLE_ADMIN") && (
+        <>
+          <Route
+            exact
+            path="/articles/edit/:id"
+            element={<ArticlesEditPage />}
+          />
+          <Route
+            exact
+            path="/articles/create"
+            element={<ArticlesCreatePage />}
+          />
+        </>
+      )}
     </Routes>
   );
 }
