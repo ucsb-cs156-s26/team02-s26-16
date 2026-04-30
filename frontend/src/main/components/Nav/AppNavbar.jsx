98f0cb2e18f360ac815d1e42711ef2aee2be6ba3
Sharanya Gehlot <sgehlot@ucsb.edu>
2026-04-29 21:46:37 -0700

    Add Articles placeholder pages and navbar link

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -71,6 +71,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/articles">
+                    Articles
+                  </Nav.Link>
                 </>
               ) : (
                 <></>
