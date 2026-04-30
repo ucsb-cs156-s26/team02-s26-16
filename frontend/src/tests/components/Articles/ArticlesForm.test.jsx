8f315829c74e37a3a81e4b1d61b4e4bcc0a08690
Sharanya Gehlot <sgehlot@ucsb.edu>
2026-04-29 17:46:50 -0700

    Fix mutation test coverage for ArticlesForm

--- frontend/src/tests/components/Articles/ArticlesForm.test.jsx
+++ frontend/src/tests/components/Articles/ArticlesForm.test.jsx
@@ -18,7 +18,13 @@ vi.mock("react-router", async () => {
 describe("ArticlesForm tests", () => {
   const queryClient = new QueryClient();
 
-  const expectedHeaders = ["Title", "URL", "Explanation", "Email", "Date Added (iso format)"];
+  const expectedHeaders = [
+    "Title",
+    "URL",
+    "Explanation",
+    "Email",
+    "Date Added (iso format)",
+  ];
   const testId = "ArticlesForm";
 
   test("renders correctly with no initialContents", async () => {
@@ -74,6 +80,24 @@ describe("ArticlesForm tests", () => {
     await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
   });
 
+  test("that all form fields have correct test ids", async () => {
+  render(
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ArticlesForm />
+      </Router>
+    </QueryClientProvider>,
+  );
+
+  expect(await screen.findByTestId(`${testId}-title`)).toBeInTheDocument();
+  expect(screen.getByTestId(`${testId}-url`)).toBeInTheDocument();
+  expect(screen.getByTestId(`${testId}-explanation`)).toBeInTheDocument();
+  expect(screen.getByTestId(`${testId}-email`)).toBeInTheDocument();
+  expect(screen.getByTestId(`${testId}-dateAdded`)).toBeInTheDocument();
+  expect(screen.getByTestId(`${testId}-submit`)).toBeInTheDocument();
+});
+
+  
   test("that the correct validations are performed", async () => {
     render(
       <QueryClientProvider client={queryClient}>
@@ -87,10 +111,10 @@ describe("ArticlesForm tests", () => {
     const submitButton = screen.getByText(/Create/);
     fireEvent.click(submitButton);
 
-    await screen.findByText(/Title is required/);
-    expect(screen.getByText(/URL is required/)).toBeInTheDocument();
-    expect(screen.getByText(/Explanation is required/)).toBeInTheDocument();
-    expect(screen.getByText(/Email is required/)).toBeInTheDocument();
-    expect(screen.getByText(/Date Added is required/)).toBeInTheDocument();
+    await screen.findByText(/Title is required./);
+    expect(screen.getByText(/URL is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Explanation is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Email is required./)).toBeInTheDocument();
+    expect(screen.getByText(/Date Added is required./)).toBeInTheDocument();
   });
 });
