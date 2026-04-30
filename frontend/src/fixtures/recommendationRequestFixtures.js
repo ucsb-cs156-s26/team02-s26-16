2b17555af80f8d4b5d88db33489cfac489062a6d
Ivy Holiday <iholiday@ucsb.edu>
2026-04-30 15:37:26 -0700

    Created recommendationRequestFixtures.js following ucsbDatesFixtures.js structure

---
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -0,0 +1,42 @@
+const recommendationRequestFixtures = {
+  oneRecommendationRequest: {
+    "id": 2,
+    "requesterEmail": "iholiday@ucsb.edu",
+    "professorEmail": "pconrad@ucsb.edu",
+    "explanation": "Need letter for BS/MS application.",
+    "dateRequested": "2026-04-30T15:28:00",
+    "dateNeeded": "2026-05-01T23:59:59",
+    "done": false
+  },
+  threeRecommendationRequests: [
+    {
+      "id": 2,
+      "requesterEmail": "iholiday@ucsb.edu",
+      "professorEmail": "pconrad@ucsb.edu",
+      "explanation": "Need letter for BS/MS application.",
+      "dateRequested": "2026-04-30T15:28:00",
+      "dateNeeded": "2026-05-01T23:59:59",
+      "done": false
+    },
+    {
+      "id": 3,
+      "requesterEmail": "iholiday@ucsb.edu",
+      "professorEmail": "pconrad@ucsb.edu",
+      "explanation": "PhD letter of recommendation needed!",
+      "dateRequested": "2026-04-30T15:28:00",
+      "dateNeeded": "2026-12-25T23:59:59",
+      "done": true
+    },
+    {
+      "id": 4,
+      "requesterEmail": "ivyholiday27@gmail.com",
+      "professorEmail": "zmatni@ucsb.edu",
+      "explanation": "Research opportunity letter of recommendation",
+      "dateRequested": "2026-04-30T14:00:00",
+      "dateNeeded": "2026-06-05T15:00:00",
+      "done": false
+    }
+  ],
+};
+
+export { recommendationRequestFixtures };
