d4573d8149fc33ecb4266e537df2481d2391c374
Sharanya Gehlot <sgehlot@ucsb.edu>
2026-04-30 17:36:28 -0700

    Add ArticlesEditPage with tests and stories

---
+++ frontend/src/stories/pages/Articles/ArticlesEditPage.stories.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { http, HttpResponse } from "msw";
+
+import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
+import { articlesFixtures } from "fixtures/articlesFixtures";
+
+export default {
+  title: "pages/Articles/ArticlesEditPage",
+  component: ArticlesEditPage,
+};
+
+const Template = () => <ArticlesEditPage storybook={true} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.get("/api/Articles", () => {
+      return HttpResponse.json(articlesFixtures.threeArticles[0], {
+        status: 200,
+      });
+    }),
+    http.put("/api/Articles", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+    http.put("/api/Articles", (req) => {
+      window.alert("PUT: " + req.url + " and body: " + req.body);
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
