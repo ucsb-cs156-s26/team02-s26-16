1d195856812e331e711d5870b3f382f926d9badc
John Sylvester <johnsylvester@ucsb.edu>
2026-05-04 22:34:35 -0700

    JS-Fixed errors in the eslint command

--- frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
+++ frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
@@ -25,11 +25,6 @@ function MenuItemReviewForm({
     /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
   // Stryker restore Regex
 
-  //const testIdPrefix = "MenuItemReviewForm";
-
-  // Stryker disable next-line all
-  const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
-
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
       <Row>
