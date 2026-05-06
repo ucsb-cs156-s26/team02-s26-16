c850b23a0e5b770198bbd17f7f3b5429c709a038
Nikita Huynh <nikitahuynh@ucsb.edu>
2026-05-05 19:10:32 -0700

    fix online again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -84,7 +84,7 @@ function App() {
         <>
           <Route
             exact
-            path="/UCSBOrganization/edit/:id"
+            path="/UCSBOrganization/edit/:orgCode"
             element={<UCSBOrganizationEditPage />}
           />
           <Route
