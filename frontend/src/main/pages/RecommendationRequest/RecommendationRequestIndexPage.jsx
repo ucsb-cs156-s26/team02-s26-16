872ca517714676a2403f73cf7405b53cde4bead2
Ivy Holiday <iholiday@ucsb.edu>
2026-05-06 23:23:35 -0700

    Removed placeholder text

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.jsx
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.jsx
@@ -39,13 +39,6 @@ export default function RecommendationRequestIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/recommendationrequest/create">Create</a>
-        </p>
-        <p>
-          <a href="/recommendationrequest/edit/1">Edit</a>
-        </p>
         {createButton()}
         <h1>Recommendation Requests</h1>
         <RecommendationRequestTable
